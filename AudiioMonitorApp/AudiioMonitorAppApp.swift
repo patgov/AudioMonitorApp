5b9ddcdd838f56eb6758224a875b9f9f90b14247
Pat Govan <pat@govango.us>
2025-03-30 07:28:50 -0500

    Initial Commit

---
+++ AudiioMonitorApp/AudiioMonitorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiioMonitorAppApp.swift
+//  AudiioMonitorApp
+//
+//  Created by Pat Govan on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AudiioMonitorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
